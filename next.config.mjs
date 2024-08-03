960f33be9dcfb63b061b59f1d475ad87e5e99128
lawtheoscar@gmial.com <lawtheoscar@gmial.com>
2024-08-03 13:22:47 +0900

    fix hostname image

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
     images:{
         remotePatterns:[
             {
-            hostname:process.env.HOSTNAME,
+            hostname:"153.92.4.56",
             }
         ]
     }
