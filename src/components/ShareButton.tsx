0afd76f47e17c54cc304f4787eaf1d4060cfae9a
scale69 <wanazmhy69@gmail.com>
2024-08-06 20:21:26 +0900

    add : xshare

--- src/components/ShareButton.tsx
+++ src/components/ShareButton.tsx
@@ -3,17 +3,18 @@ import { usePathname } from "next/navigation";
 import {
   FacebookMessengerShare,
   FacebookShare,
+  TwitterShare,
   WhatsappShare,
 } from "react-share-lite";
 export default function ShareButton() {
   const pathname = usePathname();
   const url = "https://barantara.com";
-  console.log({ pathname });
 
   return (
     <div className="w-full flex gap-2">
       <WhatsappShare size={30} url={`${url}${pathname}`} />
       <FacebookShare size={30} url={`${url}${pathname}`} />
+      <TwitterShare size={30} url={`${url}${pathname}`} />
     </div>
   );
 }
